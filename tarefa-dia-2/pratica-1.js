5e6cc0ed45e1eaf3981e80e6b585ff1bb6abff06
Daiane Oliveira <daiane.ol.santos@gmail.com>
2022-08-25 18:18:27 -0300

    atualização

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,4 +3,8 @@ let seuEmail = "daiane.ol.santos@gmail.com"
 let suaProfissao = "estudante"
 let suaIdade = 32
 let oLugarQueVoceQuerConhecer = "Islândia"
-
+console.log (seuNome)
+console.log (seuEmail)
+console.log (suaProfissao)
+console.log (suaIdade)
+console.log (oLugarQueVoceQuerConhecer)
