3088cd5949f00a0a2dbee042f991306dd187814c
Daiane Oliveira <daiane.ol.santos@gmail.com>
2022-08-26 17:34:14 -0300

    atualização-1

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -8,3 +8,5 @@ console.log (seuEmail)
 console.log (suaProfissao)
 console.log (suaIdade)
 console.log (oLugarQueVoceQuerConhecer)
+
+console.log(`Olá, ${seuNome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${oLugarQueVoceQuerConhecer}. Confirma pra gente alguns dados? A sua idade é ${suaIdade}, você é ${suaProfissao} e o seu email é ${seuEmail}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`)
